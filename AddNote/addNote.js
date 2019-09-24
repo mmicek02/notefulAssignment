e5d8d0fd233cc18badc6f917b93c8b5bf4292553
Michael Micek <49984452+mmicek02@users.noreply.github.com>
2019-09-24 06:53:20 -0500

    Updated JS file, created prop-types / error catch

---
+++ AddNote/addNote.js
@@ -0,0 +1,54 @@
+import React, {Component} from "react";
+import PropTypes from 'prop-types';
+
+class AddNote extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: {
+                value: '',
+                touched: false
+            }
+        }
+    }
+    updateName(name) {
+        this.setState({
+            name: {
+                value: name,
+                touched: true
+            }
+        });
+    }
+    handleSubmit(event) {
+        event.preventDefault();
+        const {name} = this.state;
+        console.log('Name: ', name.value);
+    }
+    validateName() {
+        const name = this.state.name.value.trim();
+        if (name.length === 0) {
+          return 'Name is required';
+        } else if (name.length < 3) {
+          return 'Name must be at least 3 characters long';
+        }
+      }
+      AddNote.propTypes = {
+        value: PropTypes.string.isRequired
+      };
+    render() {
+        return (
+            <form className='createNewNote' onSubmit={e => this.handleSubmit(e)}>
+                <label htmlFor="name">Note Name</label>
+                <input type="text" className="note_name" name="name" id="name" 
+                defaultValue="New Note" onChange={e => this.updateName(e.target.value)}/>
+                <button 
+                    type="submit" 
+                    className="newNote__button"
+                    disabled={
+                        this.validateName()
+                    }>Create Note</button>
+            </form>
+        );
+    }
+}
+export default AddNote;
---
+++ NoteListNav/NoteListNav.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import { NavLink, Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import CircleButton from '../CircleButton/CircleButton'
+import ApiContext from '../ApiContext'
+import { countNotesForFolder } from '../notes-helpers'
+import {AddFolder} from '../AddFolder/addFolder'
+import {FolderError} from '../ErrorCatch/errorCatch'
+import './NoteListNav.css'
+//import { FolderError } from '../ErrorCatch/errorCatch'
+
+export default class NoteListNav extends React.Component {
+  static contextType = ApiContext;
+
+  render() {
+    const { folders=[], notes=[] } = this.context
+    return (
+      <div className='NoteListNav'>
+        <ul className='NoteListNav__list'>
+          {folders.map(folder =>
+            <li key={folder.id}>
+              <NavLink
+                className='NoteListNav__folder-link'
+                to={`/folder/${folder.id}`}
+              >
+                <span className='NoteListNav__num-notes'>
+                  {countNotesForFolder(notes, folder.id)}
+                </span>
+                {folder.name}
+              </NavLink>
+            </li>
+          )}
+        </ul>
+        <div className='NoteListNav__button-wrapper'>
+          <FolderError>
+            <CircleButton
+              tag={Link}
+              to='/addFolder.js'
+              type='button'
+              className='NoteListNav__add-folder-button'
+            >
+              <FontAwesomeIcon icon='plus' />
+              <br />
+              Folder
+            </CircleButton>
+            <AddFolder />
+          </FolderError>
+        </div>
+      </div>
+    )
+  }
+}
